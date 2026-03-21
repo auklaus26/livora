f95f4ec1a0a6c1e50036e568acc62261169d6197
guansdu <guansdu@163.com>
2026-03-21 21:55:19 +1100

    Set up GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
